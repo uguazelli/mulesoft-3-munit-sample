4b81fb6b81c75f6f052ff241f1601679b0e8b835
Ugo Guazelli <uguazelli@deloitte.com>
2018-06-01 12:08:07 -0400

    first commit

---
+++ src/test/resources/groovy/salesforceCreate.groovy
@@ -0,0 +1,10 @@
+List list = new ArrayList();
+
+Map item1 = new HashMap();
+item1.put("id", "123456");
+item1.put("success", true);
+item1.put("errors", "{[0]}");
+
+list.add(item1);
+  
+return list;
---
+++ src/test/resources/groovy/salesforceSelect.groovy
@@ -0,0 +1,22 @@
+List list = new ArrayList();
+		
+Map item1 = new HashMap();
+item1.put("Id", "123456");
+item1.put("Name", "Name1");
+item1.put("CreatedDate", "2018-05-23T20:07:41.000Z");
+
+Map item2 = new HashMap();
+item2.put("Id", "1234567");
+item2.put("Name", "Name2");
+item2.put("CreatedDate", "2018-05-08T21:02:00.000Z");
+
+Map item3 = new HashMap();
+item3.put("Id", "12345678");
+item3.put("Name", "Name3");
+item3.put("CreatedDate", "2018-05-08T21:02:00.000Z");
+
+list.add(item1);
+list.add(item2);
+list.add(item3);
+  
+return list;
